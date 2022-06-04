ba576e2ff09baa0922e57db066b58aeadf91a34c
ocilical <32717754+ocilical@users.noreply.github.com>
2022-06-04 13:49:50 -0400

    it worksgit statusgit status now i need lighting

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ fn main() -> ImageResult<()> {
     for x in 0..CANVAS_SIZE[0] {
         for y in 0..CANVAS_SIZE[1] {
             let direction = math::canvas_to_viewport(
-                CANVAS_SIZE[0]/2 + x,
-                CANVAS_SIZE[1]/2 + y, 
+                -(CANVAS_SIZE[0] as i32)/2 + (x as i32),
+                -(CANVAS_SIZE[1] as i32)/2 + (y as i32),
                 CANVAS_SIZE,
                 scene.view_size,
                 scene.proj_plane,
